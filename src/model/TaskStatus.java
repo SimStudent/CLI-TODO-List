1a62020956455b23c96cefd9ecca6c6881e37bc1
SimStudent <738878650@qq.com>
2025-07-31 14:26:48 +0800

    feat(taskstatus): finish the task status's descriptions

--- src/model/TaskStatus.java
+++ src/model/TaskStatus.java
@@ -2,8 +2,22 @@
 package model;
 
 public enum TaskStatus{
-    TODO,
-    IN_PROGRESS,
-    DONE,
-    CANCELLED
+    // TODO,
+    // IN_PROGRESS,
+    // DONE,
+    // CANCELLED;    // It may just like some finished things.
+    TODO("The things you may not done."),
+    IN_PROGRESS("The doing things"),
+    DONE("The things is done."),
+    CANCELLED("The things is cancelled.");
+
+    private final String description;
+
+    TaskStatus(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
 }
